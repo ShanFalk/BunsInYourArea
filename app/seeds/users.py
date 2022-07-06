6855dfc68b2b11b9feb861d3ead5bee99ad41634
Shannon Falk <falk.shannon@gmail.com>
2022-07-06 19:10:25 -0400

    added aws

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,15 +4,41 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
     demo = User(
-        username='Demo', email='demo@aa.io', password='password')
-    marnie = User(
-        username='marnie', email='marnie@aa.io', password='password')
-    bobbie = User(
-        username='bobbie', email='bobbie@aa.io', password='password')
+        firstname='Demo',
+        lastname='Demo',
+        username='Demo',
+        email='demo@aa.io',
+        image_url='https://mybunnybucket.s3.amazonaws.com/61AC409D-3C44-4620-B495-A58A09AFC9F1_1_201_a.jpeg',
+        biography='Follow the white rabbit.',
+        city='Pittsburgh',
+        state='PA',
+        password='password')
+
+    celeste = User(
+        firstname='Celeste',
+        lastname='Star',
+        username='Dreamer1',
+        email='celeste@aa.io',
+        image_url='https://www.ecowatch.com/wp-content/uploads/2021/10/1641264230-origin.jpg',
+        biography='Curiouser and curiouser.',
+        city='Baltimore',
+        state='MD',
+        password='password')
+
+    benny = User(
+        firstname='Ben',
+        lastname='Berry',
+        username='its_benny',
+        email='benny@aa.io',
+        image_url='https://www.rollingstone.com/wp-content/uploads/2018/06/rs-14816-elton-1800-1395760049.jpg',
+        biography='It\'s no use going back to yesterday because I was a different person then.',
+        city='Memphis',
+        state='TN',
+        password='password')
 
     db.session.add(demo)
-    db.session.add(marnie)
-    db.session.add(bobbie)
+    db.session.add(celeste)
+    db.session.add(benny)
 
     db.session.commit()
 

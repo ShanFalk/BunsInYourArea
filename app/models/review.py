0a3610b57beefbfaf746da7964c5b55bf9c1ebab
Shannon Falk <falk.shannon@gmail.com>
2022-07-07 12:12:19 -0400

    changed favicon and started review model

---
+++ app/models/review.py
@@ -0,0 +1,7 @@
+from sqlalchemy import ForeignKey
+from .db import db
+from datetime import datetime
+from sqlalchemy.orm import validates
+
+# class Review(db.Model):
+#     __tablename__ = 'reviews'

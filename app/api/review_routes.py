8e2591abd6d5c32a6929945138816455a3ff7227
Shannon Falk <falk.shannon@gmail.com>
2022-07-09 20:24:27 -0400

    added post review form and route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, jsonify, session, request
 from flask_login import login_required
 from app.models import Review, db
 from sqlalchemy.orm import joinedload
+from app.forms import CreateReview
+from app.utils import format_errors
 
 review_routes = Blueprint('review_routes', __name__)
 
@@ -9,3 +11,24 @@ review_routes = Blueprint('review_routes', __name__)
 def user_reviews():
     reviews = Review.query.options(joinedload('reviewer')).all()
     return {review.id: review.to_dict(reviewer=review.reviewer) for review in reviews}
+
+@login_required
+@review_routes.route('')
+def create_review():
+
+    form = CreateReview()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        new_review = Review(
+            reviewer_id=form.data["reviewer_id"],
+            reviewee_id=form.data["reviewee_id"],
+            rating=form.data["rating"],
+            content=form.data["content"]
+        )
+        db.session.add(new_review)
+        db.session.commit()
+
+        review = Review.query.options(joinedload('reviewer')).get(new_review.id)
+        return review.to_dict(reviewer=review.reviewer)
+    return {'errors': format_errors(form.errors)}, 401
--- react-app/src/store/review.js
+++ react-app/src/store/review.js
@@ -21,11 +21,23 @@ export const createReview = (payload) => async (dispatch) => {
         } = payload
 
     const form = new FormData();
-    form.append()
+    form.append('reviewer_id', reviewer_id)
+    form.append('reviewee_id', reviewee_id)
+    form.append('rating', rating)
+    form.append('content', content)
 
     const response = await fetch('/api/reviews', {
-
-    })
+        method: 'POST',
+        body: form
+    });
+    if (response.ok) {
+        const data = await response.json();
+        if (data.errors) {
+            return;
+        }
+        dispatch(createOne(data));
+        return data
+    }
 }
 
 export const getReviews = () => async (dispatch) => {
@@ -48,6 +60,8 @@ export default function reviewReducer(state = initialState, action) {
         case GET_REVIEWS:
             const reviews = action.reviews
             return {...state, ...reviews}
+        case POST_REVIEW:
+            return {...state, [action.review.id]: action.review}
         default:
             return state;
     }

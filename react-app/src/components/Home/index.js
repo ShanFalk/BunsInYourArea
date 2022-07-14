f55a50c7ed166dafc72544cfe07aee5a5d0468d2
Shannon Falk <falk.shannon@gmail.com>
2022-07-14 19:21:55 -0400

    added adoptable tag

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -17,30 +17,34 @@ function BunniesList() {
     useEffect(() => {
         dispatch(getLikes(sessionUser?.id))
 
-    },[dispatch])
+    }, [dispatch])
 
     const localBunnies = bunnies.filter((bunny) => {
-        return  sessionUser.city === bunny.user.city && sessionUser.state === bunny.user.state;
+        return sessionUser.city === bunny.user.city && sessionUser.state === bunny.user.state;
     })
 
     return (
         <div>
             <h1>Bunnies near {sessionUser.city}, {sessionUser.state}</h1>
             <div className='bunnies-container'>
-            {localBunnies.map((bunny) => {
-                return (
-                    <div key={bunny.id} className='thumbnail' >
-                        <Link className='no-decor' to={`/bunnies/${bunny.id}`}>
-                            <img className='thumbnail-image' alt='a cute bunny' src={bunny.image_url}/>
-                        <ul className='thumbnail-details'>
-                            <li>Name: {bunny.name}</li>
-                            <li>Breed: {bunny.breed}</li>
-                        </ul>
-                    </Link>
-                    <LikesButton likes={likes} bunny={bunny} sessionUser={sessionUser}/>
-                    </div>
-                )
-            })}
+                {localBunnies.map((bunny) => {
+                    return (
+                        <div key={bunny.id} className='thumbnail' >
+                            <Link className='no-decor' to={`/bunnies/${bunny.id}`}>
+                                <img className='thumbnail-image' alt='a cute bunny' src={bunny.image_url} />
+
+                                <ul className='thumbnail-details'>
+                                    <li>Name: {bunny.name}</li>
+                                    <li>Breed: {bunny.breed}</li>
+                                    {bunny.is_adoptable && (
+                                        <li className='adopt'>adopt me!</li>
+                                    )}
+                                </ul>
+                            </Link>
+                            <LikesButton likes={likes} bunny={bunny} sessionUser={sessionUser} />
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )

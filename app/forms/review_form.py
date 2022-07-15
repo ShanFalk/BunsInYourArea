6ad7f6c347117237bdecabcb220ff737bb27d408
Shannon Falk <falk.shannon@gmail.com>
2022-07-15 12:40:54 -0400

    added bio input limit error handling

--- app/forms/review_form.py
+++ app/forms/review_form.py
@@ -1,12 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import TextAreaField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+
+def review_length_max(form, field):
+    content = field.data
+    if len(content) > 500:
+        raise ValidationError('Review content is too long')
 
 class CreateReview(FlaskForm):
     reviewer_id = IntegerField('reviewer_id', validators=[DataRequired()])
     reviewee_id = IntegerField('reviewee_id', validators=[DataRequired()])
     rating = IntegerField('rating', validators=[DataRequired(message="You must give a rating")])
-    content = TextAreaField('content', validators=[DataRequired(message="You must write something")])
+    content = TextAreaField('content', validators=[DataRequired(message="You must write something"), review_length_max])
 
 class UpdateReview(FlaskForm):
     id = IntegerField("id")

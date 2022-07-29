76bdccf53f290d286bcc22d168118987e739631d
Shannon Falk <falk.shannon@gmail.com>
2022-07-28 20:17:33 -0400

    improved profile rendering

--- react-app/src/store/user.js
+++ react-app/src/store/user.js
@@ -1,9 +1,14 @@
 const GET_USER = 'user/GET_USER';
+const REMOVE_USER = 'user/REMOVE_USER';
 
 const getOne = (user) => ({
     type: GET_USER,
     user
 });
+
+export const clearUser = () => ({
+    type: REMOVE_USER
+})
 
 export const getUser = (id) => async (dispatch) => {
     const response = await fetch(`/api/users/${id}`);
@@ -25,6 +30,8 @@ export default function userReducer(state = initialState, action) {
         case GET_USER:
             const user = action.user
             return {...state, ...user}
+        case REMOVE_USER:
+            return {};
         default:
             return state;
     }

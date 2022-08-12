9d61bc24b29a6f123cc9dc56162a17936f4064b1
Shannon Falk <falk.shannon@gmail.com>
2022-08-12 18:58:36 -0400

    added conversation and message seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -3,6 +3,8 @@ from .users import seed_users, undo_users
 from .bunnies import seed_bunnies, undo_bunnies
 from .reviews import seed_reviews, undo_reviews
 from .likes import seed_likes, undo_likes
+from .conversations import seed_conversations, undo_conversations
+from .messages import seed_messages, undo_messages
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -16,6 +18,8 @@ def seed():
     seed_bunnies()
     seed_reviews()
     seed_likes()
+    seed_conversations()
+    seed_messages()
     # Add other seed functions here
 
 
@@ -26,4 +30,6 @@ def undo():
     undo_bunnies()
     undo_reviews()
     undo_likes()
+    undo_conversations()
+    undo_messages()
     # Add other undo functions here

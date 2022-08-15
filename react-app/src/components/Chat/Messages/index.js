4e515f3cc7379167dfe48cfadb04b965c1826499
Shannon Falk <falk.shannon@gmail.com>
2022-08-15 13:35:38 -0400

    styled conversations page

--- react-app/src/components/Chat/Messages/index.js
+++ react-app/src/components/Chat/Messages/index.js
@@ -1,8 +1,8 @@
-import React, {useEffect, useState} from "react";
-import {useSelector, useDispatch} from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { useParams, useLocation } from "react-router-dom";
 import { io } from 'socket.io-client';
-import {clearMessages, getAllMessages} from '../../../store/message';
+import { clearMessages, getAllMessages } from '../../../store/message';
 
 
 //socket variable
@@ -12,13 +12,13 @@ function Messages() {
     const messageState = useSelector(state => Object.values(state.messages));
     const sessionUser = useSelector(state => state.session.user);
     const [tempMessages, setTempMessages] = useState([]);
-    const {conversationId} = useParams();
+    const { conversationId } = useParams();
     const id = parseInt(conversationId);
     const dispatch = useDispatch();
 
     //control form input
     const [chatInput, setChatInput] = useState("");
-
+    console.log(chatInput.length)
     useEffect(() => {
         //on mount
         dispatch(getAllMessages(id));
@@ -42,7 +42,7 @@ function Messages() {
         //when component unmounts, leave the room and disconnect
         return () => {
             dispatch(clearMessages())
-            socket.emit("leave", {conversation: conversationId })
+            socket.emit("leave", { conversation: conversationId })
             socket.disconnect()
         }
     }, []);
@@ -54,18 +54,19 @@ function Messages() {
     const sendChat = (e) => {
         e.preventDefault()
         //emit message, first arg must match backend, second arg is the data we want to send
-        socket.emit("chat", { sender: {id: sessionUser.id, username: sessionUser.username, image_url: sessionUser.image_url}, content: chatInput, conversation: conversationId });
+        socket.emit("chat", { sender: { id: sessionUser.id, username: sessionUser.username, image_url: sessionUser.image_url }, content: chatInput, conversation: conversationId });
         //clear the input field after the message is sent
         setChatInput("");
     }
 
     return (
         <div>
+            <div className='scroll-div'>
             {[...messageState, ...tempMessages].map((message, idx) => {
                 return (
                     <div key={idx} className="convo-container">
                         <div>
-                            <img src={message.sender.image_url} alt="user" className="chat-profile-pic"/>
+                            <img src={message.sender.image_url} alt="user" className="chat-profile-pic" />
                         </div>
                         <div className="msg-container">
                             <p className="username">{message.sender.username}</p>
@@ -74,15 +75,21 @@ function Messages() {
                     </div>
                 )
             })}
-            <div>
-                    <form onSubmit={sendChat}>
-                        <input
-                            value ={chatInput}
-                            onChange={updateChatInput}
-                        />
-                        <button type="submit">Send</button>
-                    </form>
-                </div>
+            </div>
+            <div className="chat-div">
+                <form onSubmit={sendChat}>
+                    <input
+                        value={chatInput}
+                        onChange={updateChatInput}
+                        className="hidden-input"
+                        required
+                        maxLength="1000"
+                        type="text"
+                    />
+                    <button className="button blue plane" type="submit"><i class="fa-solid fa-paper-plane"></i></button>
+                </form>
+            </div>
+            <p className="max-length">*maximum message length 1000 characters</p>
         </div>
     )
 }
